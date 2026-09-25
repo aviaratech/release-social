7d6910b3c363384649f7d3fd6c0b8097f44bd8d1
Paul Savignano <Paul.Savignano@gmail.com>
2026-09-25 15:36:32 -0700

    fix(core): simplify fallback markdown regexes

--- src/core/fallback.ts
+++ src/core/fallback.ts
@@ -52,8 +52,8 @@ function cleanInlineMarkdown(value: string): string {
       .replace(/\*\*([^*]+)\*\*/g, '$1')
       .replace(/__([^_]+)__/g, '$1')
       .replace(/~~([^~]+)~~/g, '$1')
-      .replace(/[\*_~]/g, '')
-      .replace(/\\([\\`*{}\[\]()#+\-.!_>])/g, '$1')
+      .replace(/[*_~]/g, '')
+      .replace(/\\([\\`*{}[\]()#+\-.!_>])/g, '$1')
       .replace(/\s+/g, ' ')
       .trim(),
   );
